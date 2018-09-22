ac5c74f82a22abb822ba1e8e2c6b4f24bde03e8d
BlameDeng <476961779@qq.com>
2018-09-22 21:36:46 +0800

    购物车选择状态逻辑

--- src/pages/cart/cart.js
+++ src/pages/cart/cart.js
@@ -10,20 +10,79 @@ import url from 'js/api.js'
 Vue.config.productionTip = false
 
 import qs from 'qs'
+import mixin from 'js/mixin.js'
 
 new Vue({
     el: '#app',
-    data: { cartLists: null },
+    data: {
+        cartLists: null,
+        total: 0
+    },
+    mixins: [mixin],
+    computed: {
+        allSelected: {
+            get() {
+                if (this.cartLists && this.cartLists.length) {
+                    return this.cartLists.every(shop => shop.checked);
+                }
+                return true;
+            },
+            set(val) {
+                this.cartLists.forEach(shop => {
+                    shop.checked = val;
+                    shop.goodsList.forEach(goods => {
+                        goods.checked = val;
+                    })
+                })
+            }
+        },
+        selectedLists() {
+            if (this.cartLists && this.cartLists.length) {
+                let arr = [];
+                let total = 0;
+                this.cartLists.forEach(shop => {
+                    shop.goodsList.forEach(goods => {
+                        if (goods.checked) {
+                            arr.push(goods);
+                            total += goods.price*goods.number;
+                        }
+                    })
+                });
+                this.total = total;
+                return arr;
+            }
+            return [];
+        }
+    },
     created() {
         let { id } = qs.parse(location.search.substr(1));
         request({ url: url.cartList }).then(res => {
-            this.cartLists = res.cartList;
-            console.log(res.cartList);
+            let arr = [];
+            res.cartList.forEach(item => {
+                item.checked = true;
+                item.goodsList.forEach(goods => {
+                    goods.checked = true;
+                });
+                arr.push(item);
+
+            });
+            this.cartLists = arr;
+            console.log(this.cartLists);
         }).catch(err => {})
     },
-    methods:{
-        onClickCheck(goods){
-            
+    methods: {
+        onClickGoods(goods, shop) {
+            goods.checked = !goods.checked;
+            shop.checked = shop.goodsList.every(goods => goods.checked)
+        },
+        onClickShop(shop) {
+            shop.checked = !shop.checked;
+            shop.goodsList.forEach(goods => {
+                goods.checked = shop.checked;
+            })
+        },
+        onSelectedAll() {
+            this.allSelected = !this.allSelected;
         }
     }
 })
